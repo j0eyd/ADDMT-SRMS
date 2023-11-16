60b7cdca04d218824de41f91c9c684e474732743
beyolimi <97134484+beyolimi@users.noreply.github.com>
2023-11-15 17:43:41 -0800

    Win32 Timer is an example project within the solution

--- Project1/TempMain.cpp
+++ Project1/TempMain.cpp
@@ -6,13 +6,18 @@ int main(int argc, char* argv[])
 	LARGE_INTEGER CounterFreq;
 	QueryPerformanceFrequency(&CounterFreq);
 	int64_t PerfCounterFreq = CounterFreq.QuadPart;
+
 	LARGE_INTEGER Start;
 	QueryPerformanceCounter(&Start);
+
 	fgetc(stdin);
+
 	LARGE_INTEGER End;
 	QueryPerformanceCounter(&End);
+
 	real32 Result = (real32)((real64)(End.QuadPart - Start.QuadPart) / (real64)PerfCounterFreq);
 	printf("%e", Result);
+
 	fgetc(stdin);
 
 }
