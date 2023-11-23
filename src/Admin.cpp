f255dcf1a50d7e536f8f66f26895de75d04f26e1
Xiaoyu Luo <luoxia@oregonstate.edu>
2023-11-22 17:26:04 -0800

    Some update about admin

--- src/Admin.cpp
+++ src/Admin.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include "Admin.h"
 #include "User.h"
 
 class Admin : public User{
@@ -10,19 +10,50 @@ public:
         const std::string& firstName, const std::string& lastName)
         : User(ID, username, password, firstName, lastName) {}
 
-    //Method to create a profile
-    void createProfile(int userType, const std::string& uID, const std::string& uName, const std::string& pWord){
-        //note: requires database interaction
+    void Admin::createStudentProfile(const Student& student) {
+        // Add student to data storage (e.g., database, file)
+        std::cout << "Student profile created for: " << student.getFirstName() << std::endl;
     }
 
-    //Method to delete a profile
-    void deleteProfile(const std::string& uID){
-        //note: requires database interaction
+    void Admin::createTeacherProfile(const Teacher& teacher) {
+        // Add teacher to data storage
+        std::cout << "Teacher profile created for: " << teacher.getFirstName() << std::endl;
     }
 
-    //Method to modify a profile
-    void modifyProfile(const std::string& uID){
-        //note: requires database interaction
+    void Admin::deleteProfile(const std::string& userID) {
+        // Remove user (student/teacher) from data storage
+        std::cout << "Profile deleted for user ID: " << userID << std::endl;
+    }
+
+    void Admin::modifyStudentProfile(const std::string& userID, const Student& newStudentData) {
+        // Update student information in data storage
+        std::cout << "Student profile updated for user ID: " << userID << std::endl;
+    }
+
+    void Admin::modifyTeacherProfile(const std::string& userID, const Teacher& newTeacherData) {
+        // Update teacher information in data storage
+        std::cout << "Teacher profile updated for user ID: " << userID << std::endl;
+    }
+
+    void Admin::enrollStudentInCourse(Student& student, Course& course) {
+        student.addEnrolledCourse(course);
+        std::cout << "Student " << student.getFirstName() << " enrolled in course: " << course.getName() << std::endl;
+    }
+
+    void Admin::unenrollStudentFromCourse(Student& student, Course& course) {
+        student.dropEnrolledCourse(course);
+        std::cout << "Student " << student.getFirstName() << " unenrolled from course: " << course.getName() << std::endl;
+    }
+
+    void Admin::assignGradeToStudent(Student& student, Grade& grade) {
+        student.addGrade(grade);
+        std::cout << "Grade assigned to student " << student.getFirstName() << std::endl;
+    }
+
+    void Admin::modifyStudentGrade(Student& student, Grade& newGradeData) {
+        // Assume we can identify the grade to modify
+        // This would require more detailed implementation
+        std::cout << "Student grade modified for " << student.getFirstName() << std::endl;
     }
 
     //Method to manually save a local backup file
